e4918c7929dc1b3d3fb4640b06994111ffacb004
masayoshi.itou <mael-strom@clear.ocn.ne.jp>
2022-03-29 22:01:02 +0900

    getAllMessagesを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -7,9 +7,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+
+//JPQL と呼ばれる少し特殊なSQL文（SELECT文）
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+    )
+})
+
 @Table(name = "messages")
 public class Message {
     @Id
